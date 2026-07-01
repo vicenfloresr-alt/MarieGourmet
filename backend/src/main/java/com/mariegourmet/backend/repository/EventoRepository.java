b01e5793c76bbfaaa99661dc10db68be914082b4
vicenfloresr-alt <vicen.floresr@duocuc.cl>
2026-07-01 16:29:35 -0400

    Delete backend directory

--- backend/src/main/java/com/mariegourmet/backend/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mariegourmet.backend.repository;
-
-import com.mariegourmet.backend.model.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-    List<Evento> findAllByOrderByFechaAsc();
-}
--- backend/src/main/java/com/mariegourmet/backend/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mariegourmet.backend.repository;
-
-import com.mariegourmet.backend.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    List<Producto> findByActivoTrueOrderByCategoriaAscNombreAsc();
-}
